b69ed220979ac30da1a75bb32f077c71e2174c7c
Ubuntu <ubuntu@node-2.novalocal>
2017-02-26 20:38:17 +0000

    initial commit of my-web-server cookbook

---
+++ my-web-server/README.md
@@ -0,0 +1,4 @@
+# my-web-server
+
+TODO: Enter the cookbook description here.
+
---
+++ my-web-server/recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: my-web-server
+# Recipe:: default
+#
+# Copyright:: 2017, The Authors, All Rights Reserved.
